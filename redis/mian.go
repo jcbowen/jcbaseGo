956c7f4d4ce4fad96e2422526a9f023e139b78e2
jcbowen <bowen@jiuchet.com>
2022-10-27 17:24:38 +0800

    新增 GetRedis 获取指定db的redis实例；优化 redisConfig

--- redis/mian.go
+++ redis/mian.go
@@ -10,16 +10,31 @@ import (
 
 var Ctx = context.Background()
 var Rdb *redis.Client
+var Config jcbaseGo.RedisStruct
 
 func init() {
-	redisConfig := jcbaseGo.Config.Get().Redis
+	Config = jcbaseGo.Config.Get().Redis
 	Rdb = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%s", redisConfig.Host, redisConfig.Port),
-		Password: redisConfig.Password,
-		DB:       helper.ToInt(redisConfig.Db),
+		Addr:     fmt.Sprintf("%s:%s", Config.Host, Config.Port),
+		Password: Config.Password,
+		DB:       helper.ToInt(Config.Db),
 	})
 	_, err := Rdb.Ping(Ctx).Result()
 	if err != nil {
 		panic(err)
 	}
+}
+
+// GetRedis 获取指定db的redis实例
+func GetRedis(db any) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", Config.Host, Config.Port),
+		Password: Config.Password,
+		DB:       helper.ToInt(db),
+	})
+	_, err := rdb.Ping(Ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	return rdb
 }
