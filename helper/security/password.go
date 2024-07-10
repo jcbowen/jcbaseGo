1719fac8cc3bcbcf71dcf52b78fbb580a752751c
Bowen <bowen@jiuchet.com>
2024-07-10 08:51:37 +0800

    调整 security为包

--- helper/security/password.go
+++ helper/security/password.go
@@ -1,12 +1,14 @@
-package helper
+package security
 
 import "golang.org/x/crypto/bcrypt"
 
+// PasswordHash hashes the password
 func PasswordHash(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
+// PasswordVerify verifies the password
 func PasswordVerify(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
