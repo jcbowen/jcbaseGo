aae856e13c335e1516b4398e84e17e9a4eecfcab
Bowen <bowen@jiuchet.com>
2024-07-15 22:18:40 +0800

    完成 redis升级

--- redis/mian.go
+++ redis/mian.go
@@ -2,10 +2,13 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/jcbowen/jcbaseGo"
 	"github.com/jcbowen/jcbaseGo/helper"
+	"time"
 )
 
 const Nil = redis.Nil
@@ -19,17 +22,14 @@ type Instance struct {
 	Context context.Context
 	Conf    jcbaseGo.RedisStruct
 	Client  *redis.Client
-	Errors  []error
 }
 
+// New 获取新的redis连接
 func New(conf jcbaseGo.RedisStruct) *Instance {
 	instance := &Instance{}
 
 	err := helper.CheckAndSetDefault(&conf)
-	if err != nil {
-		instance.AddError(err)
-		return instance
-	}
+	jcbaseGo.PanicIfError(err)
 
 	newClient := redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%s", conf.Host, conf.Port),
@@ -39,32 +39,43 @@ func New(conf jcbaseGo.RedisStruct) *Instance {
 	ctx := context.Background()
 	_, err = newClient.Ping(ctx).Result()
 
+	jcbaseGo.PanicIfError(err)
+
 	instance.Context = ctx
 	instance.Client = newClient
 	instance.Conf = conf
-	instance.AddError(err)
 
 	return instance
 }
 
+// GetClient 获取redis client
 func (i *Instance) GetClient() *redis.Client {
 	return i.Client
 }
 
-func (i *Instance) AddError(err error) {
-	if err != nil {
-		i.Errors = append(i.Errors, err)
+// GetString 根据键值，返回string
+func (i *Instance) GetString(key string, defaultValue string) (string, error) {
+	value, err := i.Client.Get(i.Context, key).Result()
+	if !errors.Is(err, Nil) || err != nil {
+		return defaultValue, err
 	}
+	return value, nil
 }
 
-func (i *Instance) Error() []error {
-	// 过滤掉c.Errors中的nil
-	var errs []error
-	for _, err := range i.Errors {
-		if err != nil {
-			errs = append(errs, err)
-		}
+// GetStruct 根据键值，返回自定义结构体类型
+func (i *Instance) GetStruct(key string, result *interface{}) error {
+	value, err := i.Client.Get(i.Context, key).Result()
+	if !errors.Is(err, Nil) || err != nil {
+		return err
 	}
 
-	return errs
+	err = json.Unmarshal([]byte(value), result)
+	return err
+}
+
+// Set 设置键值
+func (i *Instance) Set(key string, value interface{}, expire time.Duration) error {
+	jsonString, _ := json.Marshal(value)
+	err := i.Client.Set(i.Context, key, string(jsonString), expire).Err()
+	return err
 }
