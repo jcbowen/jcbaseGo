2e418ba9b63134a867040c0c7071b1568a8d9e46
Bowen <bowen@jiuchet.com>
2024-07-22 23:21:41 +0800

    完善

--- component/trait/crud.go
+++ component/trait/crud.go
@@ -2,26 +2,73 @@ package trait
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jcbowen/jcbaseGo/component/mysql"
+	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"reflect"
+	"strconv"
 	"time"
 )
 
 type CRUDTrait struct {
+	Model          any // 模型指针
+	MysqlMain      *mysql.Instance
 	PkId           string `default:"id"` // 数据表主键
-	Model          any    // 模型指针
 	ModelTableName string // 模型表名
 	OperateTime    string // 操作时间
 }
 
 func (t *CRUDTrait) ActionList(c *gin.Context) {
-	t.checkInit(c)
-	log.Println(t.ModelTableName)
+	t.checkInit()
+
+	// 获取分页参数
+	pageStr := c.DefaultQuery("page", "1")
+	pageSizeStr := c.DefaultQuery("page_size", "10")
+	page, _ := strconv.Atoi(pageStr)
+	pageSize, _ := strconv.Atoi(pageSizeStr)
+
+	// 获取排序参数
+	sortBy := c.DefaultQuery("sort_by", t.PkId)
+	order := c.DefaultQuery("order", "asc")
+
+	// 构建查询
+	query := t.MysqlMain.GetDb().Table(t.ModelTableName)
+
+	// 处理回调函数
+	query = t.ListRow(query)
+
+	var results []map[string]interface{}
+	err := query.Select("*").Order(sortBy + " " + order).Offset((page - 1) * pageSize).Limit(pageSize).Find(&results).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	total := int64(0)
+	err = query.Count(&total).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, gin.H{
+		"data":      results,
+		"page":      page,
+		"page_size": pageSize,
+		"total":     total,
+	})
+}
+
+func (t *CRUDTrait) ListRow(query *gorm.DB) *gorm.DB {
+	// 可以在这里添加用户自定义的查询逻辑
+	return query
 }
 
 // ----- 私有方法 ----- /
 
-func (t *CRUDTrait) checkInit(c *gin.Context) {
+func (t *CRUDTrait) checkInit() {
 	// 判断模型是否为空
 	if t.Model == nil {
 		log.Panic("模型不能为空")
