b4a2ce227936ace6435fbfcea1d98e5160fba085
Bowen <bowen@jiuchet.com>
2024-07-27 18:41:09 +0800

    完善 数据表基础模型

--- base_mysql.go
+++ base_mysql.go
@@ -4,51 +4,89 @@ import (
 	"github.com/jcbowen/jcbaseGo/component/helper"
 	"gorm.io/gorm"
 	"os"
+	"reflect"
+	"strings"
 	"time"
 )
 
 // MysqlBaseModel gorm基础模型
 type MysqlBaseModel struct {
+	//Id        uint   `gorm:"column:id;type:INT(11) UNSIGNED;primaryKey;autoIncrement" json:"id"`
+	//UpdatedAt string `gorm:"column:updated_at;type:DATETIME;default:NULL;comment:更新时间" json:"updated_at"`
+	//CreatedAt string `gorm:"column:created_at;type:DATETIME;default:NULL;comment:创建时间" json:"created_at"`
+	//DeletedAt string `gorm:"column:deleted_at;type:DATETIME;index;default:NULL;comment:删除时间" json:"deleted_at"`
 }
 
-func (base *MysqlBaseModel) ConfigAlias() string {
+func (b *MysqlBaseModel) ConfigAlias() string {
 	return "db"
 }
 
-func (base *MysqlBaseModel) GetTableName(modelName string) string {
+func (b *MysqlBaseModel) ModelParse(modelType reflect.Type) (tableName string, fields []string) {
+	// ----- 获取数据表名称 ----- /
 	var dbConfig DbStruct
-	dbConfigStr := os.Getenv("jc_mysql_" + base.ConfigAlias())
+	dbConfigStr := os.Getenv("jc_mysql_" + b.ConfigAlias())
 	helper.JsonString(dbConfigStr).ToStruct(&dbConfig)
 
 	// 获取表前缀
 	prefix := dbConfig.TablePrefix
 
 	// 转换为小写字母并添加下划线
-	tableName := helper.NewStr(modelName).ConvertCamelToSnake()
+	convertModelName := helper.NewStr(modelType.Name()).ConvertCamelToSnake()
 
 	if !dbConfig.SingularTable {
-		tableName += "s"
+		convertModelName += "s"
 	}
 
-	return prefix + tableName
+	tableName = prefix + convertModelName
+
+	// ----- 获取数据表所有字段 ----- /
+	fields = []string{}
+	for i := 0; i < modelType.NumField(); i++ {
+		field := modelType.Field(i)
+		gormTag := field.Tag.Get("gorm")
+		columnName := getColumnFromTag(gormTag)
+		if columnName != "" {
+			fields = append(fields, columnName)
+		} else if field.Name != "MysqlBaseModel" {
+			// 如果没有定义gorm标签，则使用字段名称转换为下划线格式
+			fields = append(fields, helper.NewStr(field.Name).ConvertCamelToSnake())
+		}
+	}
+
+	return
 }
 
-type MysqlModel struct {
-	Id        uint   `gorm:"column:id;type:INT(11) UNSIGNED;primaryKey;autoIncrement" json:"id"`
-	UpdatedAt string `gorm:"column:updated_at;type:DATETIME;default:NULL;comment:更新时间" json:"updated_at"`       // 更新时间
-	CreatedAt string `gorm:"column:created_at;type:DATETIME;default:NULL;comment:创建时间" json:"created_at"`       // 创建时间
-	DeletedAt string `gorm:"column:deleted_at;type:DATETIME;index;default:NULL;comment:删除时间" json:"deleted_at"` // 删除时间
-	MysqlBaseModel
-}
-
-func (m *MysqlModel) BeforeCreate(tx *gorm.DB) (err error) {
+func (b *MysqlBaseModel) BeforeCreate(tx *gorm.DB) (err error) {
 	strTime := time.Now().Format("2006-01-02 15:04:05")
-	m.CreatedAt = strTime
-	m.UpdatedAt = strTime
+	setFieldIfExist(tx.Statement.Dest, "CreatedAt", strTime)
+	setFieldIfExist(tx.Statement.Dest, "UpdatedAt", strTime)
 	return
 }
 
-func (m *MysqlModel) BeforeUpdate(tx *gorm.DB) (err error) {
-	m.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+func (b *MysqlBaseModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	setFieldIfExist(tx.Statement.Dest, "UpdatedAt", time.Now().Format("2006-01-02 15:04:05"))
 	return
+}
+
+func getColumnFromTag(tag string) string {
+	tags := strings.Split(tag, ";")
+	for _, t := range tags {
+		if strings.HasPrefix(t, "column:") {
+			return strings.TrimPrefix(t, "column:")
+		}
+	}
+	return ""
+}
+
+func setFieldIfExist(model interface{}, fieldName string, value string) {
+	modelValue := reflect.ValueOf(model)
+
+	if modelValue.Kind() == reflect.Ptr {
+		modelValue = modelValue.Elem()
+	}
+
+	field := modelValue.FieldByName(fieldName)
+	if field.IsValid() && field.CanSet() && field.Kind() == reflect.String {
+		field.SetString(value)
+	}
 }
--- base_sqllite.go
+++ base_sqllite.go
@@ -4,47 +4,65 @@ import (
 	"github.com/jcbowen/jcbaseGo/component/helper"
 	"gorm.io/gorm"
 	"os"
+	"reflect"
 	"time"
 )
 
-// SQLLiteModel gorm基础模型
-type SQLLiteModel struct {
-	Id        uint   `gorm:"column:id;type:INTEGER;primaryKey;autoIncrement" json:"id"`
-	UpdatedAt string `gorm:"column:updated_at;type:STRING;default:NULL" json:"updated_at"`       // 更新时间
-	CreatedAt string `gorm:"column:created_at;type:STRING;default:NULL" json:"created_at"`       // 创建时间
-	DeletedAt string `gorm:"column:deleted_at;type:STRING;index;default:NULL" json:"deleted_at"` // 删除时间
+// SQLLiteBaseModel gorm基础模型
+type SQLLiteBaseModel struct {
+	//Id        uint   `gorm:"column:id;type:INTEGER;primaryKey;autoIncrement" json:"id"`
+	//UpdatedAt string `gorm:"column:updated_at;type:STRING;default:NULL" json:"updated_at"`
+	//CreatedAt string `gorm:"column:created_at;type:STRING;default:NULL" json:"created_at"`
+	//DeletedAt string `gorm:"column:deleted_at;type:STRING;index;default:NULL" json:"deleted_at"`
 }
 
-func (m *SQLLiteModel) ConfigAlias() string {
+func (b *SQLLiteBaseModel) ConfigAlias() string {
 	return "main"
 }
 
-func (m *SQLLiteModel) GetTableName(modelName string) string {
+func (b *SQLLiteBaseModel) ModelParse(modelType reflect.Type) (tableName string, fields []string) {
+	// ----- 获取数据表名称 ----- /
 	var dbConfig SqlLiteStruct
-	dbConfigStr := os.Getenv("jc_sql_lite_" + m.ConfigAlias())
+	dbConfigStr := os.Getenv("jc_sql_lite_" + b.ConfigAlias())
 	helper.JsonString(dbConfigStr).ToStruct(&dbConfig)
 
 	// 获取表前缀
 	prefix := dbConfig.TablePrefix
 
 	// 转换为小写字母并添加下划线
-	tableName := helper.NewStr(modelName).ConvertCamelToSnake()
+	convertModelName := helper.NewStr(modelType.Name()).ConvertCamelToSnake()
 
 	if !dbConfig.SingularTable {
-		tableName += "s"
+		convertModelName += "s"
 	}
 
-	return prefix + tableName
-}
+	tableName = prefix + convertModelName
 
-func (m *SQLLiteModel) BeforeCreate(tx *gorm.DB) (err error) {
-	strTime := time.Now().Format("2006-01-02 15:04:05")
-	m.CreatedAt = strTime
-	m.UpdatedAt = strTime
+	// ----- 获取数据表所有字段 ----- /
+	fields = []string{}
+	for i := 0; i < modelType.NumField(); i++ {
+		field := modelType.Field(i)
+		gormTag := field.Tag.Get("gorm")
+		columnName := getColumnFromTag(gormTag)
+		if columnName != "" {
+			fields = append(fields, columnName)
+		} else if field.Name != "SQLLiteBaseModel" {
+			// 如果没有定义gorm标签，则使用字段名称转换为下划线格式
+			fields = append(fields, helper.NewStr(field.Name).ConvertCamelToSnake())
+		}
+	}
+
 	return
 }
 
-func (m *SQLLiteModel) BeforeUpdate(tx *gorm.DB) (err error) {
-	m.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+func (b *SQLLiteBaseModel) BeforeCreate(tx *gorm.DB) (err error) {
+	strTime := time.Now().Format("2006-01-02 15:04:05")
+	setFieldIfExist(tx.Statement.Dest, "CreatedAt", strTime)
+	setFieldIfExist(tx.Statement.Dest, "UpdatedAt", strTime)
+	return
+}
+
+func (b *SQLLiteBaseModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	setFieldIfExist(tx.Statement.Dest, "UpdatedAt", time.Now().Format("2006-01-02 15:04:05"))
 	return
 }
