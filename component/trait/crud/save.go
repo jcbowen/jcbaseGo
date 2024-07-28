5fb89b6c6ee1b53ec8f056dafdf63d3c3ee077e1
Bowen <bowen@jiuchet.com>
2024-07-28 11:48:05 +0800

    调整 方法位置

--- component/trait/crud/save.go
+++ component/trait/crud/save.go
@@ -1,13 +1,11 @@
 package crud
 
 import (
-	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/jcbowen/jcbaseGo/component/helper"
 	"github.com/jcbowen/jcbaseGo/component/security"
 	"log"
 	"reflect"
-	"strconv"
 )
 
 func (t *Trait) ActionSave(c *gin.Context) {
@@ -64,74 +62,5 @@ func (t *Trait) BeforeSave(modelValue interface{}, mapData map[string]any) (inte
 // AfterSave 保存后
 func (t *Trait) AfterSave(modelValue interface{}) error {
 	// 可以在此处添加一些后置处理逻辑
-	return nil
-}
-
-// BindMapToStruct 将 map 数据绑定到 struct，并处理类型转换
-func (t *Trait) BindMapToStruct(mapData map[string]any, modelValue interface{}) error {
-	val := reflect.ValueOf(modelValue)
-	if val.Kind() != reflect.Ptr || val.IsNil() {
-		return errors.New("modelValue must be a non-nil pointer")
-	}
-
-	modelVal := val.Elem()
-	modelType := modelVal.Type()
-
-	for i := 0; i < modelType.NumField(); i++ {
-		field := modelType.Field(i)
-		fieldName := field.Tag.Get("json")
-		if fieldName == "" {
-			fieldName = field.Name
-		}
-
-		if val, ok := mapData[fieldName]; ok {
-			fieldVal := modelVal.Field(i)
-			if !fieldVal.CanSet() {
-				log.Printf("Field %s cannot be set\n", fieldName)
-				continue
-			}
-
-			err := t.setValue(fieldVal, val)
-			if err != nil {
-				log.Printf("Error setting field %s: %v\n", fieldName, err)
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-// setValue 根据字段类型设置值
-func (t *Trait) setValue(fieldVal reflect.Value, val interface{}) error {
-	switch fieldVal.Kind() {
-	case reflect.String:
-		strVal, ok := val.(string)
-		if !ok {
-			return errors.New("cannot convert to string")
-		}
-		fieldVal.SetString(strVal)
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		intVal, err := strconv.ParseInt(val.(string), 10, 64)
-		if err != nil {
-			return err
-		}
-		fieldVal.SetInt(intVal)
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		uintVal, err := strconv.ParseUint(val.(string), 10, 64)
-		if err != nil {
-			return err
-		}
-		fieldVal.SetUint(uintVal)
-	case reflect.Float32, reflect.Float64:
-		floatVal, err := strconv.ParseFloat(val.(string), 64)
-		if err != nil {
-			return err
-		}
-		fieldVal.SetFloat(floatVal)
-	default:
-		return errors.New("unsupported field type")
-	}
-
 	return nil
 }
