2519e0cf1427d19d95d571890271acf544e08ca1
Bowen <bowen@jiuchet.com>
2024-09-17 13:05:50 +0800

    新增 文件类型，文件大小限制

--- component/trait/attachment/file.go
+++ component/trait/attachment/file.go
@@ -12,6 +12,7 @@ import (
 	_ "image/png"  // 导入 PNG 支持
 	"io"
 	"log"
+	"math"
 	"mime"
 	"mime/multipart"
 	"os"
@@ -25,7 +26,7 @@ type Attachment struct {
 	Opt *Options // 附件实例化时的参数选项
 
 	// 临时
-	FileHeader *multipart.FileHeader
+	TmpFileHeader *multipart.FileHeader
 
 	FileType       string // 附件类型
 	FileName       string // 附件名
@@ -45,6 +46,49 @@ type Options struct {
 	FileData      interface{} // 文件数据，支持base64字符串或*multipart.FileHeader
 	FileType      string      // 文件类型，默认为image
 	AttachmentDir string      // 附件目录，默认为attachment
+	MaxSize       int64       // 最大文件大小
+	AllowExt      []string    // 允许的文件扩展名
+}
+
+// typeInfo 附件类型信息
+type typeInfo struct {
+	TypeName string   // 类型名
+	MaxSize  int64    // 最大文件大小
+	AllowExt []string // 允许的文件扩展名
+}
+
+// Types 附件类型
+var Types = map[string]*typeInfo{
+	"image": {
+		TypeName: "图片",
+		AllowExt: []string{"gif", "jpg", "jpeg", "bmp", "png", "ico"},
+		MaxSize:  5 * 1024 * 1024, // 5MB
+	},
+	"voice": {
+		TypeName: "音频",
+		AllowExt: []string{"mp3", "wma", "wav", "amr"},
+		MaxSize:  50 * 1024 * 1024, // 50MB
+	},
+	"video": {
+		TypeName: "视频",
+		AllowExt: []string{"rm", "rmvb", "wmv", "avi", "mpg", "mpeg", "mp4"},
+		MaxSize:  300 * 1024 * 1024, // 300MB
+	},
+	"office": {
+		TypeName: "办公文件",
+		AllowExt: []string{
+			"wps", "wpt", "doc", "dot", "docx", "docm", "dotm",
+			"et", "ett", "xls", "xlt", "xlsx", "xlsm", "xltx", "xltm", "xlsb",
+			"dps", "dpt", "ppt", "pps", "pot", "pptx", "ppsx", "potx",
+			"txt", "csv", "prn", "pdf", "xml",
+		},
+		MaxSize: 50 * 1024 * 1024, // 50MB
+	},
+	"zip": {
+		TypeName: "压缩文件",
+		AllowExt: []string{"zip", "rar"},
+		MaxSize:  500 * 1024 * 1024, // 500MB
+	},
 }
 
 // New 创建一个新的附件实例，处理初始化和文件类型解析
@@ -57,24 +101,35 @@ func New(opt *Options) *Attachment {
 
 // initOpt 初始化选项，提供默认值设置
 func (a *Attachment) initOpt(opt *Options) {
-	if opt.FileType == "" {
-		opt.FileType = "image"
-	}
-	a.FileType = opt.FileType
+	a.Opt = opt
 
-	if opt.AttachmentDir == "" {
-		opt.AttachmentDir = "attachment"
+	if a.Opt.FileType == "" {
+		a.Opt.FileType = "image"
+	}
+	a.FileType = a.Opt.FileType
+
+	if a.Opt.AttachmentDir == "" {
+		a.Opt.AttachmentDir = "attachment"
 	}
 
-	a.saveDir = fmt.Sprintf("./%s/%ss/%s/", opt.AttachmentDir, opt.FileType, time.Now().Format("2006/01"))
+	a.saveDir = fmt.Sprintf("./%s/%ss/%s/", a.Opt.AttachmentDir, a.Opt.FileType, time.Now().Format("2006/01"))
 	a.saveDir, _ = filepath.Abs(a.saveDir)
 
-	a.Opt = opt
+	// 判断是否为文件限制补充默认配置
+	typeDefaultInfo, ok := Types[a.Opt.FileType]
+	if ok {
+		if a.Opt.MaxSize == 0 && typeDefaultInfo.MaxSize > 0 {
+			a.Opt.MaxSize = typeDefaultInfo.MaxSize
+		}
+		if len(a.Opt.AllowExt) == 0 && len(typeDefaultInfo.AllowExt) > 0 {
+			a.Opt.AllowExt = typeDefaultInfo.AllowExt
+		}
+	}
 }
 
 // Save 保存文件到指定的附件目录
 func (a *Attachment) Save() *Attachment {
-	if len(a.errors) > 0 {
+	if a.HasError() {
 		return a
 	}
 
@@ -84,7 +139,7 @@ func (a *Attachment) Save() *Attachment {
 			a.errors = append(a.errors, fmt.Errorf("无效的参数，文件为空"))
 			return a
 		} else {
-			a.FileHeader = v
+			a.TmpFileHeader = v
 		}
 	case string:
 		// 处理base64编码的文件数据
@@ -113,8 +168,23 @@ func (a *Attachment) Save() *Attachment {
 		return a
 	}
 
+	a.getExt()
+	if len(a.Opt.AllowExt) > 0 && !helper.InArray(a.FileExt, a.Opt.AllowExt) {
+		a.errors = append(a.errors, fmt.Errorf("不支持的文件【%s】", a.FileExt))
+		return a
+	}
+
+	// 赋值文件大小
+	a.FileSize = a.TmpFileHeader.Size
+
+	// 判断文件大小是否超出限制
+	if a.Opt.MaxSize > 0 && a.FileSize > a.Opt.MaxSize {
+		a.errors = append(a.errors, fmt.Errorf("文件大小不能超出[%s]", a.formatFileSize(a.Opt.MaxSize)))
+		return a
+	}
+
 	// 打开源文件（已经是临时文件的内容）
-	srcFile, err := os.Open(a.FileHeader.Filename) // 直接打开临时文件
+	srcFile, err := os.Open(a.TmpFileHeader.Filename) // 直接打开临时文件
 	if err != nil {
 		log.Println("打开文件失败: ", err)
 		a.addError(err)
@@ -128,9 +198,6 @@ func (a *Attachment) Save() *Attachment {
 			log.Println(err)
 		}
 	}(srcFile)
-
-	// 赋值文件大小
-	a.FileSize = a.FileHeader.Size
 
 	// 如果是图片，应当获取宽高
 	if a.Opt.FileType == "image" {
@@ -192,19 +259,10 @@ func (a *Attachment) Save() *Attachment {
 	return a
 }
 
-// Error 返回第一个捕获的错误
-func (a *Attachment) Error() error {
-	if len(a.errors) == 0 {
-		return nil
-	} else {
-		return a.errors[0]
-	}
-}
-
 // getExt 获取文件扩展名，若未设置则从文件名解析
 func (a *Attachment) getExt() string {
 	if a.FileExt == "" {
-		a.FileExt = strings.ToLower(filepath.Ext(a.FileHeader.Filename))
+		a.FileExt = strings.ToLower(filepath.Ext(a.TmpFileHeader.Filename))
 	}
 	return a.FileExt
 }
@@ -214,7 +272,7 @@ func (a *Attachment) fileRandomName(dir string) (filename, fullDstFile string, e
 	for {
 		dateStr := time.Now().Format("02150405")
 		randomStr := helper.Random(22)
-		filename = fmt.Sprintf("%s%s%s", dateStr, randomStr, a.FileExt)
+		filename = fmt.Sprintf("%s%s%s", dateStr, randomStr, a.getExt())
 		fullDstFile = filepath.Join(dir, filename)
 		if _, err = os.Stat(fullDstFile); os.IsNotExist(err) {
 			break
@@ -225,7 +283,7 @@ func (a *Attachment) fileRandomName(dir string) (filename, fullDstFile string, e
 	return
 }
 
-// parseBase64ToMultipart 解析base64字符串到multipart.FileHeader
+// parseBase64ToMultipart 解析base64字符串到multipart.TmpFileHeader
 func (a *Attachment) parseBase64ToMultipart(base64Data string) (*os.File, error) {
 	parts := strings.SplitN(base64Data, ",", 2)
 	if len(parts) != 2 || !strings.HasPrefix(parts[0], "data:") {
@@ -266,7 +324,7 @@ func (a *Attachment) parseBase64ToMultipart(base64Data string) (*os.File, error)
 		a.FileExt = exts[0] // 使用 mime 提供的第一个扩展名
 	}
 
-	tmpFile, err := os.CreateTemp("", "attachment-*"+a.FileExt)
+	tmpFile, err := os.CreateTemp("", "attachment-*"+a.getExt())
 	if err != nil {
 		return tmpFile, err
 	}
@@ -281,8 +339,8 @@ func (a *Attachment) parseBase64ToMultipart(base64Data string) (*os.File, error)
 		return nil, err
 	}
 
-	// 这里手动设置 Filename 为临时文件的路径，确保 FileHeader 可以正确打开文件
-	a.FileHeader = &multipart.FileHeader{
+	// 这里手动设置 Filename 为临时文件的路径，确保 TmpFileHeader 可以正确打开文件
+	a.TmpFileHeader = &multipart.FileHeader{
 		Filename: tmpFile.Name(), // 设置 Filename 为 tmpFile.Name()
 		Size:     int64(len(decodedData)),
 	}
@@ -290,9 +348,55 @@ func (a *Attachment) parseBase64ToMultipart(base64Data string) (*os.File, error)
 	return tmpFile, nil
 }
 
+// formatFileSize 格式化文件大小，将字节单位转换为适当的单位（KB, MB, GB等）并保留两位小数。
+func (a *Attachment) formatFileSize(size int64) string {
+	const (
+		KB = 1024
+		MB = 1024 * KB
+		GB = 1024 * MB
+		TB = 1024 * GB
+	)
+
+	ceilToTwoDecimals := func(num float64) float64 {
+		return math.Ceil(num*100) / 100
+	}
+
+	switch {
+	case size >= TB:
+		return fmt.Sprintf("%.2f TB", ceilToTwoDecimals(float64(size)/float64(TB)))
+	case size >= GB:
+		return fmt.Sprintf("%.2f GB", ceilToTwoDecimals(float64(size)/float64(GB)))
+	case size >= MB:
+		return fmt.Sprintf("%.2f MB", ceilToTwoDecimals(float64(size)/float64(MB)))
+	case size >= KB:
+		return fmt.Sprintf("%.2f KB", ceilToTwoDecimals(float64(size)/float64(KB)))
+	default:
+		return fmt.Sprintf("%d Bytes", size)
+	}
+}
+
 // addError 添加错误到错误列表
 func (a *Attachment) addError(err error) {
 	if err != nil {
 		a.errors = append(a.errors, err)
 	}
+}
+
+// HasError 是否有错误
+func (a *Attachment) HasError() bool {
+	return a.HasError()
+}
+
+// Error 返回第一个捕获的错误
+func (a *Attachment) Error() error {
+	if len(a.errors) == 0 {
+		return nil
+	} else {
+		return a.errors[0]
+	}
+}
+
+// Errors 返回所有捕获的错误
+func (a *Attachment) Errors() []error {
+	return a.errors
 }
