552d7cd657c74fd2297e3edf8bc5b6b0b1d6073a
Bowen <bowen@jiuchet.com>
2025-05-20 15:36:32 +0800

    优化 跨域配置

--- middleware/main.go
+++ middleware/main.go
@@ -28,15 +28,12 @@ func (b Base) Cors() gin.HandlerFunc {
 			headerStr = "Access-Control-Allow-Origin, Access-Control-Allow-Headers"
 		}
 		if origin != "" {
-			origin := c.Request.Header.Get("Origin")
-			c.Header("Access-Control-Allow-Origin", origin)                                    // 这是允许访问所有域
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
-			// header的类型
-			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma, Code")
-			// 允许跨域设置 可以返回其他子段
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
-			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
-			c.Header("Access-Control-Allow-Credentials", "true")                                                                                                                                                   // 跨域请求是否需要带cookie信息 默认设置为true
+			c.Header("Access-Control-Allow-Origin", origin)                                                          // 动态设置允许的域名
+			c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD, CONNECT, TRACE") //允许所有方法
+			c.Header("Access-Control-Allow-Headers", "*")                                                            //允许所有请求头
+			c.Header("Access-Control-Expose-Headers", "*")                                                           //允许所有响应头
+			c.Header("Access-Control-Max-Age", "172800")                                                             // 缓存请求信息 单位为秒
+			c.Header("Access-Control-Allow-Credentials", "true")                                                     // 跨域请求是否需要带cookie信息 默认设置为true
 			// c.Set("Content-type", "application/json;charset=utf-8")                                                                                                                                             // 设置返回格式是json（已调整到crud包的基础文件中）
 
 			// 添加 CSP 头部
