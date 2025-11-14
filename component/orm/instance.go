4d3a2aa3ead3233280fa5756b185d484db6c742f
Bowen <bowen@jiuchet.com>
2025-11-14 22:11:37 +0800

    新增(orm) 添加SetDebuggerLogger和GetDebuggerLogger方法

--- component/orm/instance.go
+++ component/orm/instance.go
@@ -1,6 +1,7 @@
 package orm
 
 import (
+	"github.com/jcbowen/jcbaseGo/component/debugger"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,8 @@ type Instance interface {
 	GetDb() *gorm.DB
 	// GetConf 获取数据库配置信息，类型：jcbaseGo.DbStruct 或 jcbaseGo.SqlLiteStruct
 	GetConf() interface{}
+	// SetDebuggerLogger 设置debugger日志记录器
+	SetDebuggerLogger(debuggerLogger debugger.LoggerInterface)
+	// GetDebuggerLogger 获取debugger日志记录器
+	GetDebuggerLogger() debugger.LoggerInterface
 }
